e5ca2b0ffc135a143462341f8e5d500e8a3f451f
renkx <m@renkx.com>
2024-02-25 02:27:29 +0800

    update

--- install_docker.sh
+++ install_docker.sh
@@ -264,10 +264,7 @@ if [[ ! -f /etc/rc.local ]]; then
 # 加载内核配置，否则会被覆盖
 sysctl --system
 
-cat >/etc/resolv.conf <<'EOF'
-nameserver 127.0.0.1
-nameserver 8.8.8.8
-EOF
+echo "nameserver 127.0.0.1\nnameserver 8.8.8.8" > /etc/resolv.conf
 
 exit 0
 EOF
