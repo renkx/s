a8a47f7642a5b48c77aec71d42a3cad4515db5b3
renkx <m@renkx.com>
2024-02-26 17:39:48 +0800

    update

--- docker.sh
+++ docker.sh
@@ -172,15 +172,12 @@ net.ipv4.tcp_fin_timeout = 15
 net.ipv4.ip_local_port_range = 1024 65535
 net.ipv4.tcp_max_tw_buckets = 5000
 net.ipv4.tcp_fastopen = 3
-net.ipv4.tcp_rmem = 4096 87380 67108864
-net.ipv4.tcp_wmem = 4096 65536 67108864
 net.ipv4.udp_rmem_min = 8192
 net.ipv4.udp_wmem_min = 8192
 net.ipv4.tcp_mtu_probing = 1
 net.ipv4.tcp_autocorking = 0
 net.ipv4.tcp_slow_start_after_idle = 0
 net.ipv4.tcp_max_syn_backlog = 819200
-net.ipv4.tcp_notsent_lowat = 16384
 net.ipv4.tcp_no_metrics_save = 0
 net.ipv4.tcp_ecn = 1
 net.ipv4.tcp_ecn_fallback = 1
@@ -212,6 +209,13 @@ net.netfilter.nf_conntrack_tcp_timeout_last_ack=10
 net.core.default_qdisc = fq_pie
 net.ipv4.tcp_congestion_control = bbr
 net.ipv4.tcp_low_latency = 1
+# https://blog.cloudflare.com/optimizing-tcp-for-high-throughput-and-low-latency/#cloudflare-production-network-results
+# https://github.com/cloudflare/linux/tree/master/patches
+net.ipv4.tcp_rmem = 8192 262144 536870912
+net.ipv4.tcp_wmem = 4096 16384 536870912
+net.ipv4.tcp_adv_win_scale = -2
+net.ipv4.tcp_collapse_max_bytes = 6291456
+net.ipv4.tcp_notsent_lowat = 131072
 EOF
 
   # 载入配置使其生效
