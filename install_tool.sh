219a3c7047ce88ff229213fdd7fa063e6ab509d7
renkx <m@renkx.com>
2024-03-12 01:23:20 +0800

    update

--- install_tool.sh
+++ install_tool.sh
@@ -17,6 +17,22 @@ Info="${Green}[信息]${Font}"
 OK="${Green}[OK]${Font}"
 Error="${Red}[错误]${Font}"
 
+# 直接开始系统优化
+export startOptimizing='0'
+
+while [[ $# -ge 1 ]]; do
+	case $1 in
+	--startOptimizing)
+		shift
+		startOptimizing="1"
+		;;
+	*)
+		echo -ne "\nInvaild option: '$1'\n\n";
+		exit 1
+		;;
+	esac
+done
+
 echo_info() {
   # shellcheck disable=SC2145
   echo -e "${Info} ${GreenBG} $@ ${Font}"
@@ -225,39 +241,82 @@ optimizing_system() {
 
 # 覆盖写入
   cat >'/etc/sysctl.d/optimizing-sysctl.conf' <<EOF
-# https://github.com/leitbogioro/Tools/blob/master/README.md
-net.ipv4.ip_local_port_range = 1024 65535
-net.core.rmem_max = 536870912
-net.core.wmem_max = 536870912
-net.core.somaxconn = 32768
-net.core.netdev_max_backlog = 32768
-net.ipv4.tcp_max_tw_buckets = 65536
-net.ipv4.tcp_abort_on_overflow = 1
-net.ipv4.tcp_slow_start_after_idle = 0
-net.ipv4.tcp_timestamps = 1
-net.ipv4.tcp_syncookies = 0
-net.ipv4.tcp_syn_retries = 3
-net.ipv4.tcp_synack_retries = 3
-net.ipv4.tcp_max_syn_backlog = 32768
-net.ipv4.tcp_fin_timeout = 15
-net.ipv4.tcp_keepalive_intvl = 3
-net.ipv4.tcp_keepalive_probes = 5
-net.ipv4.tcp_keepalive_time = 600
-net.ipv4.tcp_retries1 = 3
-net.ipv4.tcp_retries2 = 5
-net.ipv4.tcp_no_metrics_save = 1
+net.ipv4.tcp_fack = 1
+net.ipv4.tcp_early_retrans = 3
+net.ipv4.neigh.default.unres_qlen=10000
+net.ipv4.conf.all.route_localnet=1
 net.ipv4.ip_forward = 1
-fs.file-max = 104857600
-fs.inotify.max_user_instances = 8192
-fs.nr_open = 1048576
-# 有好有坏？
-net.ipv4.tcp_tw_reuse = 1
-net.ipv4.tcp_tw_recycle = 0
+net.ipv4.conf.all.forwarding = 1
+net.ipv4.conf.default.forwarding = 1
+#net.ipv6.conf.all.forwarding = 1  #awsipv6问题
+net.ipv6.conf.default.forwarding = 1
+net.ipv6.conf.lo.forwarding = 1
+net.ipv6.conf.all.disable_ipv6 = 0
+net.ipv6.conf.default.disable_ipv6 = 0
+net.ipv6.conf.lo.disable_ipv6 = 0
+net.ipv6.conf.all.accept_ra = 2
+net.ipv6.conf.default.accept_ra = 2
+net.core.netdev_max_backlog = 100000
+net.core.netdev_budget = 50000
+net.core.netdev_budget_usecs = 5000
+#fs.file-max = 51200
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.rmem_default = 67108864
+net.core.wmem_default = 67108864
+net.core.optmem_max = 65536
+net.core.somaxconn = 1000000
+net.ipv4.icmp_echo_ignore_all = 0
+net.ipv4.icmp_echo_ignore_broadcasts = 1
+net.ipv4.icmp_ignore_bogus_error_responses = 1
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0
+net.ipv4.conf.all.secure_redirects = 0
+net.ipv4.conf.default.secure_redirects = 0
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+net.ipv4.conf.default.rp_filter = 0
+net.ipv4.conf.all.rp_filter = 0
+net.ipv4.tcp_keepalive_time = 600
+net.ipv4.tcp_keepalive_intvl = 15
+net.ipv4.tcp_keepalive_probes = 2
+net.ipv4.tcp_synack_retries = 1
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_rfc1337 = 0
+net.ipv4.tcp_timestamps = 1
+net.ipv4.tcp_tw_reuse = 0
+net.ipv4.tcp_fin_timeout = 15
+net.ipv4.ip_local_port_range = 1024 65535
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_fastopen = 3
+net.ipv4.udp_rmem_min = 8192
+net.ipv4.udp_wmem_min = 8192
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_autocorking = 0
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_max_syn_backlog = 819200
+net.ipv4.tcp_no_metrics_save = 0
+net.ipv4.tcp_ecn = 1
+net.ipv4.tcp_ecn_fallback = 1
+net.ipv4.tcp_frto = 0
+net.ipv6.conf.all.accept_redirects = 0
+net.ipv6.conf.default.accept_redirects = 0
+net.ipv4.neigh.default.gc_thresh3=8192
+net.ipv4.neigh.default.gc_thresh2=4096
+net.ipv4.neigh.default.gc_thresh1=2048
+net.ipv6.neigh.default.gc_thresh3=8192
+net.ipv6.neigh.default.gc_thresh2=4096
+net.ipv6.neigh.default.gc_thresh1=2048
+net.ipv4.tcp_orphan_retries = 1
+net.ipv4.tcp_retries2 = 5
+vm.swappiness = 1
+vm.overcommit_memory = 1
+kernel.pid_max=64000
 # Linux 连接跟踪调优
 net.netfilter.nf_conntrack_max = 2621440
 net.netfilter.nf_conntrack_tcp_timeout_established=600
 ## Enable bbr
-net.core.default_qdisc = fq_pie
+net.core.default_qdisc = fq
 net.ipv4.tcp_congestion_control = bbr
 net.ipv4.tcp_low_latency = 1
 # https://blog.cloudflare.com/optimizing-tcp-for-high-throughput-and-low-latency/#cloudflare-production-network-results
@@ -793,4 +852,9 @@ menu() {
     esac
 }
 
-menu
+# 直接开始系统优化
+if [[ "$startOptimizing" == "1" ]]; then
+  optimizing_system
+else
+  menu
+fi
