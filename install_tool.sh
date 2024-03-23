cdc7197f42945cf4f0d09a712efbac87edaafbee
renkx <m@renkx.com>
2024-03-24 02:56:30 +0800

    update

--- install_tool.sh
+++ install_tool.sh
@@ -397,10 +397,10 @@ nameserver 127.0.0.1
 nameserver 8.8.8.8
 EOF
   else
-  echo_info "114.114.114.114 设置中。。。"
+  echo_info "223.5.5.5 设置中。。。"
   cat >/etc/resolv.conf <<'EOF'
 nameserver 127.0.0.1
-nameserver 114.114.114.114
+nameserver 223.5.5.5
 EOF
   fi
 
