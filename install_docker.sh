05b0b51cba74ea24e1ba80c4f6af70e43a4238c7
renkx <m@renkx.com>
2026-01-16 14:06:32 +0800

    update

--- install_docker.sh
+++ install_docker.sh
@@ -120,38 +120,35 @@ iptables_open() {
   ip6tables -F
 }
 
-install_docker() {
-  is_root
-  check_system
+# 防火墙处理
+setup_firewall() {
+  # iptables-persistent 是一个用于在 Debian 系统上保存和恢复 iptables 防火墙规则的工具
+  # 它允许你在系统重启后保留之前设置的 iptables 规则，从而确保防火墙在重新启动后仍然有效。
+  if dpkg -l | grep -q iptables-persistent; then
+    echo_ok "防火墙已安装"
+  else
+    # 关闭所有交互界面 让包管理器使用 默认值（debconf 中预设的默认选项）来安装
+    export DEBIAN_FRONTEND=noninteractive
 
-# iptables-persistent 是一个用于在 Debian 系统上保存和恢复 iptables 防火墙规则的工具
-# 它允许你在系统重启后保留之前设置的 iptables 规则，从而确保防火墙在重新启动后仍然有效。
-if dpkg -l | grep -q iptables-persistent; then
+    echo_ok "安装防火墙，进入安装流程.."
+    iptables_open
+    remove iptables-persistent
+    check_result "卸载原有的 iptables-persistent"
 
-  echo_ok "防火墙已安装"
-else
-  # 关闭所有交互界面 让包管理器使用 默认值（debconf 中预设的默认选项）来安装
-  export DEBIAN_FRONTEND=noninteractive
+    remove ufw
+    check_result "卸载 ufw"
 
-  echo_ok "安装防火墙，进入安装流程.."
-  iptables_open
-  remove iptables-persistent
-  check_result "卸载原有的 iptables-persistent"
+    apt update -y && apt install -y iptables-persistent
+    check_result "安装 iptables-persistent"
 
-  remove ufw
-  check_result "卸载 ufw"
+    rm -f /etc/iptables/rules.v4
+    echo_ok "删除原有的 /etc/iptables/rules.v4"
 
-  apt update -y && apt install -y iptables-persistent
-  check_result "安装 iptables-persistent"
+    # 获取ssh端口
+    current_port=$(ss -tlnp | grep sshd | awk '{print $4}' | grep -oE '[0-9]+$' | head -n1)
+    current_port=${current_port:-22}
 
-  rm -f /etc/iptables/rules.v4
-  echo_ok "删除原有的 /etc/iptables/rules.v4"
-
-  # 获取ssh端口
-  current_port=$(ss -tlnp | grep sshd | awk '{print $4}' | grep -oE '[0-9]+$' | head -n1)
-  current_port=${current_port:-22}
-
-  cat > /etc/iptables/rules.v4 << EOF
+    cat > /etc/iptables/rules.v4 << EOF
 *filter
 :INPUT ACCEPT [0:0]
 :FORWARD ACCEPT [0:0]
@@ -163,127 +160,128 @@ else
 -A INPUT -p tcp --dport $current_port -j ACCEPT
 COMMIT
 EOF
-  check_result "写入iptables规则到 /etc/iptables/rules.v4"
+    check_result "写入iptables规则到 /etc/iptables/rules.v4"
 
-  iptables-restore < /etc/iptables/rules.v4
-  check_result "iptables-restore < /etc/iptables/rules.v4 使规则生效"
+    iptables-restore < /etc/iptables/rules.v4
+    check_result "iptables-restore < /etc/iptables/rules.v4 使规则生效"
 
-  systemctl enable netfilter-persistent
-  check_result "netfilter-persistent 设置开机启动"
+    systemctl enable netfilter-persistent
+    check_result "netfilter-persistent 设置开机启动"
 
-  echo_ok "防火墙安装完成"
-fi
+    echo_ok "防火墙安装完成"
+  fi
+}
 
-    echo_info "检测是否能ping谷歌"
-    IsGlobal="0"
-    delay="$(ping -4 -c 2 -w 2 www.google.com | grep rtt | cut -d'/' -f4 | awk '{ print $3 }' | sed -n '/^[0-9]\+\(\.[0-9]\+\)\?$/p')";
-    if [ "$delay" != "" ] ; then
-    	IsGlobal="1"
-    	echo_info "延迟：$delay ms , ping yes"
+install_docker() {
+  is_root
+  check_system
+  # 防火墙处理
+  setup_firewall
+
+  echo_info "检测是否能ping谷歌"
+  IsGlobal="0"
+  delay="$(ping -4 -c 2 -w 2 www.google.com | grep rtt | cut -d'/' -f4 | awk '{ print $3 }' | sed -n '/^[0-9]\+\(\.[0-9]\+\)\?$/p')";
+  if [ "$delay" != "" ] ; then
+    IsGlobal="1"
+    echo_info "延迟：$delay ms , ping yes"
+  else
+    echo_info "延迟：$delay ms , ping no"
+  fi
+
+  if [[ "${ID}" == "debian" ]]; then
+      # 参考：https://docs.docker.com/engine/install/debian/#install-using-the-repository
+    $INS update
+    $INS install -y ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    judge "安装 docker 依赖"
+
+    # 检测是国外
+    if [[ "$IsGlobal" == "1" ]];then
+
+      echo_ok "能访问国外，使用官方docker源"
+
+      # 添加官方GPG key
+      curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+      chmod a+r /etc/apt/keyrings/docker.asc
+      judge "添加官方GPG key"
+
+      # 设置源
+      echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        tee /etc/apt/sources.list.d/docker.list > /dev/null
+      judge "设置 docker 源"
+      $INS update
+      judge "更新 apt 缓存"
+      $INS install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+      judge "安装 docker"
+
+      ## 配置docker镜像加速器
+      if [ ! -d /etc/docker/ ]; then
+        mkdir -p /etc/docker
+        check_result "创建 /etc/docker/ 目录"
+      fi
+      touch /etc/docker/daemon.json
+      judge "创建 /etc/docker/daemon.json 文件"
+
+      ## "bip": "172.17.0.1/16", # docker网段设置
+      cat > /etc/docker/daemon.json << EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "1m",
+    "max-file": "3"
+  }
+}
+EOF
+      check_result "配置 /etc/docker/daemon.json"
+
+      systemctl daemon-reload && systemctl restart docker
+      judge "重启 docker 使配置生效"
+
     else
-      echo_info "延迟：$delay ms , ping no"
+
+      echo_ok "不能访问国外，使用阿里的docker源"
+      # 安装GPG证书
+      curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+      chmod a+r /etc/apt/keyrings/docker.asc
+      judge "添加GPG key"
+
+      # 设置源
+      echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://mirrors.ustc.edu.cn/docker-ce/linux/debian \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        tee /etc/apt/sources.list.d/docker.list > /dev/null
+      judge "设置 docker 源"
+      $INS update
+      judge "更新 apt 缓存"
+      $INS install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+      judge "安装 docker"
+
+      ## 配置docker镜像加速器
+      if [ ! -d /etc/docker/ ]; then
+        mkdir -p /etc/docker
+        check_result "创建 /etc/docker/ 目录"
+      fi
+      touch /etc/docker/daemon.json
+      judge "创建 /etc/docker/daemon.json 文件"
+
+      ## "bip": "172.17.0.1/16", # docker网段设置
+      cat > /etc/docker/daemon.json << EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "1m",
+    "max-file": "3"
+  }
+}
+EOF
+      check_result "配置 /etc/docker/daemon.json"
+      systemctl daemon-reload && systemctl restart docker
+      judge "重启 docker 使配置生效"
     fi
-
-    if [[ "${ID}" == "debian" ]]; then
-        # 参考：https://docs.docker.com/engine/install/debian/#install-using-the-repository
-        $INS update
-        $INS install -y ca-certificates curl
-        install -m 0755 -d /etc/apt/keyrings
-        judge "安装 docker 依赖"
-
-        # 检测是国外
-        if [[ "$IsGlobal" == "1" ]];then
-
-            echo_ok "能访问国外，使用官方docker源"
-
-            # 添加官方GPG key
-            curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-            chmod a+r /etc/apt/keyrings/docker.asc
-            judge "添加官方GPG key"
-
-            # 设置源
-            echo \
-              "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-              $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-              tee /etc/apt/sources.list.d/docker.list > /dev/null
-            judge "设置 docker 源"
-            $INS update
-            judge "更新 apt 缓存"
-            $INS install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-            judge "安装 docker"
-
-            ## 配置docker镜像加速器
-            if [ ! -d /etc/docker/ ]; then
-              mkdir -p /etc/docker
-              check_result "创建 /etc/docker/ 目录"
-            fi
-            touch /etc/docker/daemon.json
-            judge "创建 /etc/docker/daemon.json 文件"
-
-            ## "bip": "172.17.0.1/16", # docker网段设置
-            cat > /etc/docker/daemon.json << EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "1m",
-    "max-file": "3"
-  }
-}
-EOF
-            check_result "配置 docker 镜像加速器"
-
-            systemctl daemon-reload && systemctl restart docker
-            judge "重启 docker"
-            echo_ok "能访问国外，无需配置docker国内镜像加速器"
-        else
-
-            echo_ok "不能访问国外，使用阿里的docker源"
-            # 安装GPG证书
-            curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-            chmod a+r /etc/apt/keyrings/docker.asc
-            judge "添加GPG key"
-
-            # 设置源
-            echo \
-              "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://mirrors.ustc.edu.cn/docker-ce/linux/debian \
-              $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-              tee /etc/apt/sources.list.d/docker.list > /dev/null
-            judge "设置 docker 源"
-            $INS update
-            judge "更新 apt 缓存"
-            $INS install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-            judge "安装 docker"
-
-            ## 配置docker镜像加速器
-            if [ ! -d /etc/docker/ ]; then
-              mkdir -p /etc/docker
-              check_result "创建 /etc/docker/ 目录"
-            fi
-            touch /etc/docker/daemon.json
-            judge "创建 /etc/docker/daemon.json 文件"
-
-            ## "bip": "172.17.0.1/16", # docker网段设置
-            cat > /etc/docker/daemon.json << EOF
-{
-  "registry-mirrors": [
-    "https://m9wl9ue4.mirror.aliyuncs.com",
-    "https://docker.mirrors.ustc.edu.cn",
-    "https://reg-mirror.qiniu.com"
-  ],
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "1m",
-    "max-file": "3"
-  }
-}
-EOF
-            check_result "配置 docker 镜像加速器"
-            systemctl daemon-reload && systemctl restart docker
-            judge "重启 docker"
-            echo_ok "不能访问国外，已配置docker国内镜像加速器"
-        fi
 
     else
       echo_error "当前系统为 ${ID} ${VERSION_ID} 不在支持的系统列表内，安装中断"
